da1996dfc07e856fa9e96fcc48600e79cd1e2e22
Yuki Takeichi <yuki.takeichi@gmail.com>
2022-09-18 23:37:24 +0900

    https://zenn.dev/kage1020/scraps/263bea122c7ddf

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
+import { Session } from 'next-auth'
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 
-const App = ({ Component, pageProps }: AppProps) => {
+// https://zenn.dev/kage1020/scraps/263bea122c7ddf
+const App = ({ Component, pageProps }: AppProps<{ session: Session }>) => {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
